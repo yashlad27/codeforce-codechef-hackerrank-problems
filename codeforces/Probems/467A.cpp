bd17d8af9dc9174abad59acbed6e3e121a004a5c
Yash Lad <71868797+yashlad27@users.noreply.github.com>
2022-01-18 20:04:48 +0530

    Update 467A.cpp

--- codeforces/Probems/467A.cpp
+++ codeforces/Probems/467A.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
-#include <cstring>
-#include <algorithm>
-using namespace std;
 
-int main() {
-	
-	// george and accommodation: 467A
-	
-	int n; // number of rooms
-	cin>>n;
-	
-	int p,q,ans; // no. of people living in room, room's capacity
-	
-	for(int i = 0; i<n; i++)
-	{
-		cin>>p>>q;
-		if(q-p>=2)
-		{
-			ans++;
-		}
-	}
-	cout<<ans;
-	return 0;
+int main()
+{
+    int n;
+    int p, q;
+    int count{0};
+    const int Space_for_George_and_Alex{2};
+
+    std::cin >> n;
+
+    while (n--)
+    {
+        std::cin >> p >> q;
+
+        if (q - p >= Space_for_George_and_Alex)
+        {
+            ++count;
+        }
+    }
+
+    std::cout << count << "\n";
 }
