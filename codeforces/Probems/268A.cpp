1f2d369a2531d196e7b9fedb3d9dbd037def04b3
Yash Lad <71868797+yashlad27@users.noreply.github.com>
2022-01-22 20:25:55 +0530

    commit on main

---
+++ codeforces/Probems/268A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int l, r;
+    int sum = l + r;
+    int a[sum];
+    int t;
+    cin>>t;
+    int k=r-l;
+    cin>>l>>r;
+    
+    while(t--)
+    {
+        for(int i=0; i<a.length(); i++)
+        {
+            
+        }
+
+    }
+    return 0;
+}
