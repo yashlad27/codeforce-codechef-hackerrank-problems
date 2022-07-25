1b7beb8cf76613f3990001f53fd9f32ae0f28240
Yash Lad <yashlad727@gmail.com>
2022-07-25 15:16:05 +0530

    commit03

--- hrank/cpp/classesANDobj.cpp
+++ hrank/cpp/classesANDobj.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 class Student {
-    
+    private:
+    int scores[5];
+
+    public:
+    void input(){
+        for(int i=0; i<5; i++){
+            cin>>scores[i];
+        }
+    }
+    int calculateTotalScore(){
+        int total=0;
+        for(int i=0; i<5; i++){
+            total+=scores[i];
+        }
+        return total;
+    }
 };
 
 int main(){
 
+    Student s1;
+    s1.input();
+    s1.calculateTotalScore();
     return 0;
 }
