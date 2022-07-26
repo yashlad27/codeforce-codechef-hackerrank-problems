29b710de1a321b4ed8092119b10fe738b36bfefe
Yash Lad <yashlad727@gmail.com>
2022-07-26 16:20:55 +0530

    commit-01

---
+++ hrank/algo/01.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n; // number of elements in the array
+    int a[n]; 
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+        cout<<a[n];
+    }
+    return 0; 
+}
