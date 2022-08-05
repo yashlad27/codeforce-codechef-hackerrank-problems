ea75089c3160521e2a2adc04b3629c2b0a321d94
Yash Lad <yashlad727@gmail.com>
2022-08-05 17:21:59 +0530

    intial commit-01

---
+++ codeforces/Probems/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int t; // test cases
+    cin>>t;
+    while(t>0){
+        int x; // starting point
+        int n; // final point
+        cin>>n;
+    }
+    return 0;
+}
